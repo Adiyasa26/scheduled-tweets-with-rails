de8feea864e8007d89e4f885645c8f6cc23631ad
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-04-27 15:47:26 +0700

    Create a sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
